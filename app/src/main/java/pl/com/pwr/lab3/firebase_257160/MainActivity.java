1d111ab566c0c2807f68ac63d3bc6671ee9af8d7
Germain63870 <germain.lefebvre63@laposte.net>
2020-04-29 21:13:40 +0200

    First commit

--- app/src/main/java/pl/com/pwr/lab3/firebase_257160/MainActivity.java
+++ app/src/main/java/pl/com/pwr/lab3/firebase_257160/MainActivity.java
@@ -1,8 +1,12 @@
 package pl.com.pwr.lab3.firebase_257160;
 
+import android.Manifest;
+import android.content.ContentValues;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.provider.MediaStore;
@@ -17,19 +21,11 @@ import androidx.annotation.Nullable;
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.FirebaseApp;
-import com.google.firebase.ml.vision.FirebaseVision;
-import com.google.firebase.ml.vision.common.FirebaseVisionImage;
-import com.google.firebase.ml.vision.text.FirebaseVisionText;
-import com.google.firebase.ml.vision.text.FirebaseVisionTextRecognizer;
 
 import java.io.FileNotFoundException;
 import java.io.InputStream;
-import java.util.List;
 import java.util.Objects;
-import java.util.concurrent.atomic.AtomicReference;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -38,10 +34,11 @@ public class MainActivity extends AppCompatActivity {
     private Button mButton, mButton_text, mButton_camera, mButton_object;
 
     private static final int GALLERY_REQUEST_CODE = 1;
-    static final int REQUEST_IMAGE_CAPTURE = 1;
+    private static final int PERMISSION_CODE = 1000;
+    private static final int REQUEST_IMAGE_CAPTURE = 1;
+    private Uri image_uri;
 
     private Bitmap mBitmap;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -64,14 +61,29 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 dispatchTakePictureIntent();
                 textview.setText("");
+
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                    if (checkSelfPermission(Manifest.permission.CAMERA) == PackageManager.PERMISSION_DENIED || checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_DENIED) {
+                        //permission not enabled, request it
+                        String[] permission = {Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE};
+                        //show popup to request permissions
+                        requestPermissions(permission, PERMISSION_CODE);
+                    } else {
+                        //permission already granted
+                        openCamera();
+                    }
+                } else {
+                    //system os < marshmallow
+                    openCamera();
+                }
             }
         });
 
         mButton_text.setOnClickListener(new View.OnClickListener() {
+            @RequiresApi(api = Build.VERSION_CODES.KITKAT)
             @Override
             public void onClick(View v) {
-                detectTextFromImage();
-
+                //detectTextFromImage();
             }
         });
     }
@@ -111,76 +123,91 @@ public class MainActivity extends AppCompatActivity {
                 e.printStackTrace();
             }
             Bitmap bitmap = BitmapFactory.decodeStream(inputStream);
-                mSelectedImage.setImageBitmap(bitmap);
-                //Bundle extras = data.getExtras();
-                //Bitmap bitmap = (Bitmap) extras.get("data");
+            mSelectedImage.setImageBitmap(bitmap);
+            //Bundle extras = data.getExtras();
+            //Bitmap bitmap = (Bitmap) extras.get("data");
 
-                recognizeMyText(mBitmap);
+            //recognizeMyText(mBitmap);
         }
     }
 
-    private void recognizeMyText (Bitmap bitmap) {
-        FirebaseVisionImage firebaseVisionImage = FirebaseVisionImage.fromBitmap(bitmap);
-        FirebaseVisionTextRecognizer mFirebaseVisionTextRecognizer = FirebaseVision.getInstance().getOnDeviceTextRecognizer();
-        mFirebaseVisionTextRecognizer.processImage(firebaseVisionImage).addOnSuccessListener(new OnSuccessListener<FirebaseVisionText>() {
-            @Override
-            public void onSuccess(FirebaseVisionText firebaseVisionText) {
-                String text = firebaseVisionText.getText();
+    private void openCamera() {
+        ContentValues values = new ContentValues();
+        values.put(MediaStore.Images.Media.TITLE, "New Picture");
+        values.put(MediaStore.Images.Media.DESCRIPTION, "From the Camera");
+        image_uri = getContentResolver().insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values);
+        //Camera intent
+        Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, image_uri);
+        startActivityForResult(cameraIntent, REQUEST_IMAGE_CAPTURE);
+    }
 
-                if (text.isEmpty()) {
-                    Toast.makeText(MainActivity.this, "No Text Detected", Toast.LENGTH_SHORT).show();
+    //handling permission result
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        //this method is called, when user presses Allow or Deny from Permission Request Popup
+        switch (requestCode) {
+            case PERMISSION_CODE: {
+                if (grantResults.length > 0 && grantResults[0] ==
+                        PackageManager.PERMISSION_GRANTED) {
+                    //permission from popup was granted
+                    openCamera();
                 } else {
-                    // Intent intent = new Intent(MainActivity.this, ResultActivity.class);
-                    // intent.putExtra(FinalTextRecognition.FINAL_TEXT, label);
-                    // startActivity(intent);
-
-                    textview.setText(text);
+                    //permission from popup was denied
+                    Toast.makeText(this, "Permission denied...", Toast.LENGTH_SHORT).show();
                 }
             }
-        });
-    }
-
-    private void detectTextFromImage() {
-        FirebaseVisionImage firebaseVisionImage = FirebaseVisionImage.fromBitmap(mBitmap);
-        FirebaseVisionTextRecognizer mFirebaseVisionTextRecognizer = FirebaseVision.getInstance().getOnDeviceTextRecognizer();
-        mFirebaseVisionTextRecognizer.processImage(firebaseVisionImage).addOnSuccessListener(new OnSuccessListener<FirebaseVisionText>() {
-            @Override
-            public void onSuccess(FirebaseVisionText firebaseVisionText) {
-                displayTextFromImage(firebaseVisionText);
-            }
-        }).addOnFailureListener(new OnFailureListener() {
-            @Override
-            public void onFailure(@NonNull Exception e) {
-                Toast.makeText(MainActivity.this, "Error" + e.getMessage(), Toast.LENGTH_SHORT).show();
-            }
-        });
-
-    }
-
-    private void displayTextFromImage(FirebaseVisionText firebaseVisionText) {
-        List<FirebaseVisionText.TextBlock> blockList = firebaseVisionText.getTextBlocks();
-        if (blockList.size() == 0) {
-            Toast.makeText(this, "No text found in image", Toast.LENGTH_SHORT).show();
-        } else {
-            for (FirebaseVisionText.TextBlock block : firebaseVisionText.getTextBlocks()) {
-                AtomicReference<List<FirebaseVisionText.TextBlock>> text = new AtomicReference<>(firebaseVisionText.getTextBlocks());
-                textview.setText((CharSequence) text);
-            }
-
         }
     }
 
 
-    // Partie créer en regardans ML Kit de Github, + class Utils
+ //   private void recognizeMyText(Bitmap bitmap) {
+//        FirebaseVisionTextRecognizer mFirebaseVisionTextRecognizer = FirebaseVision.getInstance().getOnDeviceTextRecognizer();
+  //      mFirebaseVisionTextRecognizer.processImage(firebaseVisionImage).addOnSuccessListener(new OnSuccessListener<FirebaseVisionText>() {
+    //        @Override
+      //      public void onSuccess(FirebaseVisionText firebaseVisionText) {
+        //        String text = firebaseVisionText.getText();
+//
+  //              if (text.isEmpty()) {
+    ///                Toast.makeText(MainActivity.this, "No Text Detected", Toast.LENGTH_SHORT).show();
+       //         } else {
+         //           // Intent intent = new Intent(MainActivity.this, ResultActivity.class);
+           //         // intent.putExtra(FinalTextRecognition.FINAL_TEXT, label);
+             //       // startActivity(intent);
+//
+  //                  textview.setText(text);
+    //            }
+      //      }
+        //});
+    //}
 
-    @Override
-    protected void onResume() {
-        super.onResume();
-        if (!Utils.allPermissionsGranted(this)) {
-            Utils.requestRuntimePermissions(this);
-        }
-    }
+  //  private void detectTextFromImage() {
+     //   FirebaseVisionImage firebaseVisionImage = FirebaseVisionImage.fromBitmap(mBitmap);
+    //    FirebaseVisionTextRecognizer mFirebaseVisionTextRecognizer = FirebaseVision.getInstance().getOnDeviceTextRecognizer();
+     //   mFirebaseVisionTextRecognizer.processImage(firebaseVisionImage).addOnSuccessListener(new OnSuccessListener<FirebaseVisionText>() {
+     //       @Override
+      //      public void onSuccess(FirebaseVisionText firebaseVisionText) {
+       //         displayTextFromImage(firebaseVisionText);
+      //      }
+     //   }).addOnFailureListener(new OnFailureListener() {
+      ////      @Override
+      //      public void onFailure(@NonNull Exception e) {
+      //          Toast.makeText(MainActivity.this, "Error" + e.getMessage(), Toast.LENGTH_SHORT).show();
+      //      }
+     //   });
+
+  //  }
+
+    //private void displayTextFromImage(FirebaseVisionText firebaseVisionText) {
+       // List<FirebaseVisionText.TextBlock> blockList = firebaseVisionText.getTextBlocks();
+     //   if (blockList.size() == 0) {
+       //     Toast.makeText(this, "No text found in image", Toast.LENGTH_SHORT).show();
+       // } else {
+         //   for (FirebaseVisionText.TextBlock block : firebaseVisionText.getTextBlocks()) {
+         //       AtomicReference<List<FirebaseVisionText.TextBlock>> text = new AtomicReference<>(firebaseVisionText.getTextBlocks());
+         //       textview.setText((CharSequence) text);
+         //   }
+
+       // }
+  //  }
 }
-
-
-
